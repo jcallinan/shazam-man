cacbd8fbb6c86d0d607800c46f115da2edee6a25
jcallinan <jeremy.callinan@gmail.com>
2021-01-26 19:49:17 -0500

    First checkin of initial objects
    Describing songs, releases, playlists,etc.

---
+++ ShazamExample1/ShazamExample1/Release.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShazamExample1
+{
+    enum ReleaseType
+    {
+        Single,
+        MaxiSingle,
+        EP,
+        Album
+    }
+    enum Genre
+    {
+        Country,
+        Rap,
+        Rock,
+        EDM
+    }
+    class Release
+    {
+        public string ReleaseTitle;
+        public ReleaseType thisReleaseType;
+        public Genre thisGenre;
+        public Artist thisArtist;
+        public DateTime ReleaseDate;
+    }
+}
